7775740e70641bf7abb0f33faf18841a3c3edc75
marshmalocode <gustinmelvin6@gmail.com>
2023-01-27 11:06:11 +0100

    fr

--- public/js/captcha.js
+++ public/js/captcha.js
@@ -50,7 +50,7 @@ function captchaCF2M(callback, captchaLen) {
 
     // Génère un nombre décimal afin d'être utilisé en tant que valeur de em
     function randomSize() {
-        return randomInt(10, 18) / 10
+        return randomInt(20, 30) / 10
     }
 
     const allCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
